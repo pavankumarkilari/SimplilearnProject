85487632b5ec5104f43571a8a1e3067cc87b026d
pavankumarkilari <pavankumarkilari519@gmail.com>
2023-11-27 21:20:45 +0530

    Simplilearn project

---
+++ Phase2StudentData/ConAppStudent/ConAppStudent/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace ConAppStudent
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string FilePath = "C:\\Users\\kpava\\Desktop\\Mphasis\\Simplilearn_Projects\\Phase2StudentData\\ConAppStudent\\StudentData.txt";
+                string[] Lines = File.ReadAllLines(FilePath);
+                foreach (string Line in Lines)
+                {
+                    Console.WriteLine(Line);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error!" + ex.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
