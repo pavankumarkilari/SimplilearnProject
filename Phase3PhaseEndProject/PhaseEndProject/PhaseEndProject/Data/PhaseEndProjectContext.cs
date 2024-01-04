8511d7a43f2fbac59876b41d5c151a74300e12ec
pavankumarkilari <pavankumarkilari519@gmail.com>
2024-01-04 20:07:29 +0530

    Simplilearn Project

---
+++ Phase3PhaseEndProject/PhaseEndProject/PhaseEndProject/Data/PhaseEndProjectContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PhaseEndProject.Models;
+
+namespace PhaseEndProject.Data
+{
+    public class PhaseEndProjectContext : DbContext
+    {
+        public PhaseEndProjectContext (DbContextOptions<PhaseEndProjectContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PhaseEndProject.Models.DeptMaster> DeptMaster { get; set; } = default!;
+
+        public DbSet<PhaseEndProject.Models.EmpProfile>? EmpProfile { get; set; }
+    }
+}
---
+++ Phase3PhaseEndProject/PhaseEndProject/PhaseEndProject/Models/DeptMaster.cs
@@ -0,0 +1,15 @@
+﻿using PhaseEndProject.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PhaseEndProject.Models
+{
+    [Table("DeptMaster")]
+    public class DeptMaster
+    {
+        [Key]
+        public int DeptCode { get; set; }
+        public string DeptName { get; set; }
+        public virtual ICollection<EmpProfile> EmpProfiles { get; set; }
+    }
+}
