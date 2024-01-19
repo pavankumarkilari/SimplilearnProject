8da48215a425c71e0902e042114a0cdd6c8a362c
pavankumarkilari <pavankumarkilari519@gmail.com>
2024-01-19 13:16:29 +0530

    Simplilearn Project

---
+++ Phase4DockerProject/BankLoginDocker/BankLoginDocker/Controllers/LoginController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BankLoginDocker.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            if (username == "Pavan" && password == "Pavan@1234")
+            {
+                return RedirectToAction("Dashboard");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Invalid username or password";
+                return View("Login");
+            }
+            
+        }
+
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+        public IActionResult Logout()
+        {
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ Phase4DockerProject/BankLoginDocker/BankLoginDocker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BankLoginDocker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
